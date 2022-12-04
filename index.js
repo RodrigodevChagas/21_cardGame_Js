75c0e088c553de169eaab1dcea6ee6ae376ed877
dyggo <dyggo@DESKTOP-FJQGG3Q>
2022-12-04 00:30:45 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+document.getElementById("id").innerText = "vem hexa"
+
+let bonusPoints = 50
+console.log(bonusPoints)
+
+bonusPoints += 50
+console.log(bonusPoints)
+
+bonusPoints -= 75
+console.log(bonusPoints)
+
+bonusPoints += 45
+console.log(bonusPoints)
