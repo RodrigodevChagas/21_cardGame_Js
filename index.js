16be8376a8d2e1307feae5adc925ec2e2a8787d3
dyggo <dyggo@DESKTOP-FJQGG3Q>
2022-12-07 13:55:57 -0300

    Aprimorando projeto

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-document.getElementById("id").innerText = "vem hexa"
+//document.getElementById("id").innerText = "vem hexa"
 
-let bonusPoints = 50
-console.log(bonusPoints)
+// bonusPoints += 50
+// console.log(bonusPoints)
 
-bonusPoints += 50
-console.log(bonusPoints)
+let cardsEl = document.getElementById("cards")
+let cards = 0
+let texto = ""
 
-bonusPoints -= 75
-console.log(bonusPoints)
 
-bonusPoints += 45
-console.log(bonusPoints)
+function contaCartas(){
+    
+    cards++
+    cardsEl.textContent = `Cartas: ${cards} - ${cards} `
+    console.log(texto)
+    console.log(typeof(cards))
+    
+    if(cards === 21) texto = "Jackpot"
+    else if (cards < 21) texto = "Tire mais uma carta"
+    else {
+        texto = "Você perdeu, tente de novo"
+        comecarJogo()
+    }
+}
+
+function comecarJogo(){
+    cards = 0
+    cardsEl.textContent = cards
+}
