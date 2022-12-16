77f324f5d3ec476153df3000098d5e8d2454f2b3
dyggo <dyggo@DESKTOP-FJQGG3Q>
2022-12-16 15:32:39 -0300

    finalizando logica do jackpot

--- index.js
+++ index.js
@@ -4,26 +4,52 @@
 // console.log(bonusPoints)
 
 let cardsEl = document.getElementById("cards")
+let somaEl = document.getElementById("sum")
+let paraEl = document.getElementById("paragrafo")
 let cards = 0
 let texto = ""
-
+let cardsArray = []
+let soma = 0;
+let numeroAleatorio = 0
 
 function contaCartas(){
+    geraAleatorio()
     
-    cards++
-    cardsEl.textContent = `Cartas: ${cards} - ${cards} `
-    console.log(texto)
-    console.log(typeof(cards))
-    
-    if(cards === 21) texto = "Jackpot"
-    else if (cards < 21) texto = "Tire mais uma carta"
-    else {
-        texto = "Você perdeu, tente de novo"
-        comecarJogo()
+    if(soma === 21 || (soma>21 && texto == "Jackpot")) {
+        
+        paraEl.style.color = 'green'
+        texto == "Jackpot" ? comecarJogo() : texto = "Jackpot"
+        paraEl.textContent = texto
+    }
+    else if (soma < 21) {
+        texto = "Tire mais uma carta"
+        paraEl.textContent = texto
+    }
+    else if( soma>21 && texto != "Jackpot"){
+        paraEl.style.color = 'red'
+        texto =="Você perdeu, tente de novo" ? comecarJogo() :texto = "Você perdeu, tente de novo"
+        paraEl.textContent = texto
     }
 }
 
 function comecarJogo(){
+    cardsArray = []
     cards = 0
-    cardsEl.textContent = cards
+    soma = 0
+    texto = ""
+    cardsEl.textContent = `Cartas: `
+    somaEl.textContent = `Soma: `
+    paraEl.style.color = 'white'
+}
+function geraAleatorio(){
+   
+    numeroAleatorio = Math.floor(Math.random() * 13) + 1
+    if(cardsArray.includes(numeroAleatorio)) geraAleatorio() 
+    else 
+    {cardsArray.push(numeroAleatorio)
+    cardsEl.textContent += `${cardsArray[cards]} `
+    soma += cardsArray[cards]
+    somaEl.textContent = `Soma: ${soma} `
+    cards++
+    }
 }
